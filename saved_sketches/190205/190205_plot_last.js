6f1fb5492bef8f9cfdfeb1fbe14c6d7cdb16076b
Frank Albenesius <frankalbenesius@gmail.com>
2019-02-06 18:41:27 -0600

    store last sketch

---
+++ saved_sketches/190205/190205_plot_last.js
@@ -0,0 +1,66 @@
+const canvasSketch = require("canvas-sketch");
+const { renderPolylines } = require("canvas-sketch-util/penplot");
+const { clipPolylinesToBox } = require("canvas-sketch-util/geometry");
+const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
+
+const settings = {
+  dimensions: [1000, 1000],
+  orientation: "landscape",
+  pixelsPerInch: 300
+};
+
+const createGrid = (xCount = 60, yCount = xCount) => {
+  const points = [];
+  for (let x = 0; x < xCount; x++) {
+    for (let y = 0; y < yCount; y++) {
+      const u = xCount <= 1 ? 0.5 : x / (xCount - 1);
+      const v = xCount <= 1 ? 0.5 : y / (yCount - 1);
+      points.push({
+        noise: random.noise2D(u, v, 0.25, 3), // -1..1
+        position: [u, v]
+      });
+    }
+  }
+  return points;
+};
+
+const sketch = ({ width, height }) => {
+  const rel = Math.min(width, height);
+  const margin = rel / 15;
+  let lines = [];
+
+  const ratio = width / height;
+  const widthPoints = 40;
+  const heightPoints = widthPoints * ratio;
+  const points = createGrid(30, 100).filter(
+    point => Math.abs(point.noise) >= 0
+  );
+
+  points.forEach(point => {
+    const { position, noise } = point;
+    const [u, v] = position;
+    const lerpMargin = margin * 1.2; // so as to not clip anything too close to edge
+    const x = lerp(lerpMargin, width - lerpMargin, u);
+    const y = lerp(lerpMargin, height - lerpMargin, v);
+
+    const radius = (rel / 80) * Math.abs(noise);
+    const theta = Math.PI * 2 * Math.min(1.4, Math.max(0.95, Math.abs(noise)));
+
+    const line = [
+      [x - Math.cos(theta) * radius, y - Math.sin(theta) * radius],
+      [x + Math.cos(theta) * radius, y + Math.sin(theta) * radius]
+    ];
+    lines.push(line);
+  });
+
+  // Clip all the lines to a margin
+  const box = [margin, margin, width - margin, height - margin];
+  lines = clipPolylinesToBox(lines, box);
+
+  // The 'penplot' util includes a utility to render
+  // and export both PNG and SVG files
+  return props => renderPolylines(lines, props);
+};
+
+canvasSketch(sketch, settings);
