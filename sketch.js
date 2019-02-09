30e6e1c289f98ccaa0806d84644f2cc442a2cd1e
Frank Albenesius <frankalbenesius@gmail.com>
2019-02-08 22:23:31 -0600

    add runic large

--- sketch.js
+++ sketch.js
@@ -2,56 +2,82 @@ import canvasSketch from "canvas-sketch";
 import { renderPolylines } from "canvas-sketch-util/penplot";
 import { clipPolylinesToBox } from "canvas-sketch-util/geometry";
 import random from "canvas-sketch-util/random";
-import createGrid from "./util/createGrid";
-import { lerp } from "canvas-sketch-util/math";
+// import createGrid from "./util/createGrid";
+import { lerp, clamp } from "canvas-sketch-util/math";
 import array from "new-array";
 import convexHull from "convexhull-js";
 
 const settings = {
-  dimensions: [6, 4],
+  dimensions: [5.5, 4.25],
   units: "in",
   pixelsPerInch: 300
 };
 
-function createGlyphLines(position, meas, noise) {
-  const [x, y] = position;
-  const l = meas / 5;
-  const pointCount = random.rangeFloor(3, 8);
-  const xyPoints = array(pointCount).map(() => {
-    const [x2, y2] = random.onCircle(l * Math.abs(noise));
-    return { x: x + x2, y: y + y2 };
-  });
-  const hulledXyPoints = convexHull(xyPoints);
-  const randomIndex = random.rangeFloor(hulledXyPoints.length);
-  const rearrangedHulledXyPoints = [
-    ...hulledXyPoints.slice(randomIndex),
-    ...hulledXyPoints.slice(0, randomIndex)
-  ];
-  return rearrangedHulledXyPoints.map(({ x, y }) => [x, y]);
-}
+random.setSeed("winter");
+
+const createGrid = (xCount = 60, yCount = xCount) => {
+  const points = [];
+  for (let x = 0; x < xCount; x++) {
+    for (let y = 0; y < yCount; y++) {
+      const u = xCount <= 1 ? 0.5 : x / (xCount - 1);
+      const v = xCount <= 1 ? 0.5 : y / (yCount - 1);
+      points.push({
+        noise: random.noise2D(u, v, 0.25, 2.6), // -1..1
+        position: [u, v]
+      });
+    }
+  }
+  return points;
+};
 
 const sketch = ({ width, height }) => {
-  const meas = Math.min(width, height) / 10; // a measure for relative calculations
-
-  const rows = 35;
-  const columns = 40;
-  const margin = meas / 2;
-  const points = createGrid(rows, columns).map(([u, v]) => {
-    const x = lerp(margin * 1.5, width - margin * 1.5, u);
-    const y = lerp(margin * 1.5, height - margin * 1.5, v);
-    return {
-      position: [x, y],
-      noise: random.noise2D(u, v, 0.1, 1.5)
-    };
-  });
-
+  const rel = Math.min(width, height);
+  const margin = rel / 15;
   let lines = [];
-  points.forEach(({ position, noise }) => {
-    lines.push(createGlyphLines(position, meas, noise));
+
+  const ratio = width / height;
+  const points = createGrid(29, 40).filter(point => Math.abs(point.noise) >= 0);
+
+  points.forEach(point => {
+    const { position, noise } = point;
+    const [u, v] = position;
+    const lerpMargin = margin * 1.2; // so as to not clip anything too close to edge
+    const x = lerp(lerpMargin, width - lerpMargin, u);
+    const y = lerp(lerpMargin, height - lerpMargin, v);
+
+    const radius = (rel / 80) * Math.abs(noise);
+    const theta = Math.PI * 2 * Math.min(1.4, Math.max(0.95, Math.abs(noise)));
+
+    const line = [
+      [x - Math.cos(theta) * radius, y - Math.sin(theta) * radius],
+      [x + Math.cos(theta) * radius, y + Math.sin(theta) * radius]
+    ];
+    lines.push(line);
   });
 
+  points.forEach(point => {
+    const { position, noise } = point;
+    const [u, v] = position;
+    const lerpMargin = margin * 1.2; // so as to not clip anything too close to edge
+    const x = lerp(lerpMargin, width - lerpMargin, 1 - u);
+    const y = lerp(lerpMargin, height - lerpMargin, v);
+
+    const radius = (rel / 80) * Math.abs(noise);
+    const theta = Math.PI * -2 * Math.min(1.4, Math.max(0.95, Math.abs(noise)));
+
+    const line = [
+      [x - Math.cos(theta) * radius, y - Math.sin(theta) * radius],
+      [x + Math.cos(theta) * radius, y + Math.sin(theta) * radius]
+    ];
+    lines.push(line);
+  });
+
+  // Clip all the lines to a margin
   const box = [margin, margin, width - margin, height - margin];
   lines = clipPolylinesToBox(lines, box);
+
+  // The 'penplot' util includes a utility to render
+  // and export both PNG and SVG files
   return props => renderPolylines(lines, props);
 };
 
