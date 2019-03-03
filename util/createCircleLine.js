1597409b262afe9e46b6d6843ef190c69caa058c
Frank Albenesius <frankalbenesius@gmail.com>
2019-03-03 10:43:07 -0600

    add more dnd backs

--- util/createCircleLine.js
+++ util/createCircleLine.js
@@ -1,8 +1,8 @@
-function createCircleLine(x, y, r, steps = 30) {
+function createCircleLine(x, y, r, steps = 30, initialTheta = 0) {
   let circleLine = [];
   const theta = (Math.PI * 2) / steps;
   for (let i = 0; i <= steps; i++) {
-    const angle = theta * i;
+    const angle = initialTheta + theta * i;
     const point = [x + Math.cos(angle) * r, y + Math.sin(angle) * r];
     circleLine.push(point);
   }
