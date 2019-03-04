67240de039680200b61bcc8d98af6973bec8166e
Frank Albenesius <frankalbenesius@gmail.com>
2019-03-04 15:53:17 -0600

    saving 3dnoise progress1

---
+++ util/index.js
@@ -0,0 +1,16 @@
+// import createGrid from './createGrid'
+import createGrid from "./createGrid";
+import angleBetweenPoints from "./angleBetweenPoints";
+import createCircleLine from "./createCircleLine";
+import distanceBetweenPoints from "./distanceBetweenPoints";
+import logSeed from "./logSeed";
+import settings from "./settings";
+
+export {
+  createGrid,
+  angleBetweenPoints,
+  createCircleLine,
+  distanceBetweenPoints,
+  logSeed,
+  settings
+};
