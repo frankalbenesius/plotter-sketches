5e5a4e4970b14110066477200af3cd1d3fbbf7fc
Frank Albenesius <frankalbenesius@gmail.com>
2019-03-05 22:13:48 -0600

    add 190305 session

--- util/settings.js
+++ util/settings.js
@@ -1,4 +1,12 @@
 export default {
+  playground: {
+    dimensions: [1024, 1024]
+  },
+  postcard: {
+    dimensions: [6, 4],
+    units: "in",
+    pixelsPerInch: 300
+  },
   workScreen: {
     dimensions: [3840, 2160],
     orientation: "landscape",
