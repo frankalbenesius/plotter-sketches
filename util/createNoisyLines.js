7f8d19782bdb0a669e74260cb7d4123356c71f16
Frank Albenesius <frankalbenesius@gmail.com>
2019-03-16 13:15:54 -0500

    add three-panel bardic

---
+++ util/createNoisyLines.js
@@ -0,0 +1,86 @@
+import { clipPolylinesToBox } from "canvas-sketch-util/geometry";
+import random from "canvas-sketch-util/random";
+import { lerp } from "canvas-sketch-util/math";
+import range from "lodash/range";
+import { distanceBetweenPoints } from "./";
+
+export default function createNoisyLines(
+  pointA = [0, 0],
+  pointB = [100, 100],
+  angle = random.value() * Math.PI * 2,
+  border = false
+) {
+  let lines = [];
+
+  // define initial utility variables
+  const [ax, ay] = pointA;
+  const [bx, by] = pointB;
+  const centerX = (bx + ax) / 2;
+  const centerY = (by + ay) / 2;
+  const lineLength = distanceBetweenPoints(pointA, pointB);
+  const lineAngle = angle - Math.PI * 0.5; // "angle" represents angle of growing lines, not the line itself
+  const baseLinePadding = lineLength / 100;
+
+  // can be useful to see border for debugging
+  if (border) {
+    let borderLine = [[ax, ay], [bx, ay], [bx, by], [ax, by], [ax, ay]];
+    lines.push(borderLine);
+  }
+
+  // create the starting line at the correct radius and angle.
+  // it will start opposite the "angle" of the flow of the lines
+  // and be just wide enough to cover the entirety of the bounding box
+  const startLineMidpoint = [
+    centerX + (Math.cos(angle + Math.PI) * lineLength) / 2,
+    centerY + (Math.sin(angle + Math.PI) * lineLength) / 2
+  ];
+  const firstLineStartPoint = [
+    startLineMidpoint[0] - (Math.cos(lineAngle) * lineLength) / 2,
+    startLineMidpoint[1] - (Math.sin(lineAngle) * lineLength) / 2
+  ];
+  const firstLineEndPoint = [
+    startLineMidpoint[0] + (Math.cos(lineAngle) * lineLength) / 2,
+    startLineMidpoint[1] + (Math.sin(lineAngle) * lineLength) / 2
+  ];
+  const firstLine = range(0, 1.00000001, 0.01).map(v => {
+    const x = lerp(firstLineStartPoint[0], firstLineEndPoint[0], v);
+    const y = lerp(firstLineStartPoint[1], firstLineEndPoint[1], v);
+    return [x, y];
+  });
+
+  // plotting points just for debugging
+  // firstLine.forEach(([x, y]) => {
+  //   const pointCircle = createCircleLine(x, y, 3);
+  //   lines.push(pointCircle);
+  // });
+
+  let noisyLines = [
+    firstLine // straight & not "noisy" but will never be seen;
+  ];
+  let traveledDistance = 0;
+  const maxDistance = lineLength * 0.7; // just cause some white space is more interesting
+  while (traveledDistance < maxDistance) {
+    let newLine = [];
+    const shadowedLine = noisyLines[noisyLines.length - 1];
+    shadowedLine.forEach(([shadowedX, shadowedY]) => {
+      const noise = random.noise2D(shadowedX, shadowedY, 1); // abs so it always grows
+      const padding = baseLinePadding + (baseLinePadding / 2) * noise; // we never want it to be totally flat
+      const point = [
+        shadowedX + Math.cos(angle) * padding,
+        shadowedY + Math.sin(angle) * padding
+      ];
+      newLine.push(point);
+    });
+    noisyLines.push(newLine);
+    traveledDistance = Math.abs(
+      distanceBetweenPoints(firstLine[0], newLine[0])
+    );
+  }
+  lines.push(...noisyLines);
+
+  // clip all of the lines to the provided bounding box
+  const box = [ax, ay, bx, by];
+  lines = clipPolylinesToBox(lines, box);
+
+  return lines;
+}
