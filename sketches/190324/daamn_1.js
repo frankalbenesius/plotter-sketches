20b7122bbeb8f9a1ab34a92073f497c133f75b86
Frank Albenesius <frankalbenesius@gmail.com>
2019-03-24 10:27:33 -0500

    render circles

--- sketches/190324/daamn_1.js
+++ sketches/190324/daamn_1.js
@@ -5,48 +5,99 @@ import random from "canvas-sketch-util/random";
 import math from "canvas-sketch-util/math";
 import { settings, createGrid, createCircleLine } from "../../util";
 
-function generateBrassConfig() {
-  return [];
+const nodeOptions = [
+  {
+    kind: "circle",
+    numLeafOptions: [1]
+  }
+];
+
+function generateBrassTree(levels = 0) {
+  if (levels === 0) {
+    return null;
+  }
+
+  const nodeConfig = random.pick(nodeOptions);
+  const numLeaves = random.pick(nodeConfig.numLeafOptions);
+
+  const leafConfigs = [];
+  for (let i = 0; i < numLeaves; i++) {
+    const leafConfig = generateBrassTree(levels - 1);
+    if (leafConfig) {
+      leafConfigs.push(leafConfig);
+    }
+  }
+  return {
+    ...nodeConfig,
+    leaves: leafConfigs
+  };
 }
 
-function createBrassLines(
-  startPoint,
-  brassWidth,
-  brassConfig = generateBrassConfig()
-) {
+function createBrassTreeLines(brassTreeHead, point, width) {
+  let lines = [];
+  switch (brassTreeHead.kind) {
+    case "circle": {
+      const circleRadius = width * 0.2;
+      const circleLine = createCircleLine(
+        point[0],
+        point[1] + circleRadius,
+        circleRadius
+      );
+      lines.push(circleLine);
+
+      const numLeaves = brassTreeHead.leaves.length;
+      for (let i = 0; i < numLeaves; i++) {
+        const leafPoint = [point[0], point[1] + circleRadius * 2];
+        const leafLines = createBrassTreeLines(
+          brassTreeHead.leaves[i],
+          leafPoint,
+          width / numLeaves
+        );
+        lines.push(...leafLines);
+      }
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+  return lines;
+}
+
+function createPairBrassLines(startPoint, brassPairWidth, brassTree) {
   // final lines accumulator
   const brassLines = [];
 
-  // rendering a dot just to show the brass starting point, can remove later
-  const dotRadius = brassWidth / 40;
-  const startPointCircle = createCircleLine(
-    startPoint[0],
-    startPoint[1],
-    dotRadius
-  );
-  brassLines.push(startPointCircle);
+  // // rendering a dot just to show the brass starting point, can remove later
+  // const dotRadius = brassPairWidth / 40;
+  // const startPointCircle = createCircleLine(
+  //   startPoint[0],
+  //   startPoint[1],
+  //   dotRadius
+  // );
+  // brassLines.push(startPointCircle);
 
-  // rendering a line just to show the entire brass width, can remove later
-  const widthGuideLine = [
-    [startPoint[0] - brassWidth / 2, startPoint[1]],
-    [startPoint[0] + brassWidth / 2, startPoint[1]]
-  ];
-  brassLines.push(widthGuideLine);
+  // // rendering a line just to show the entire brass width, can remove later
+  // const widthGuideLine = [
+  //   [startPoint[0] - brassPairWidth / 2, startPoint[1]],
+  //   [startPoint[0] + brassPairWidth / 2, startPoint[1]]
+  // ];
+  // brassLines.push(widthGuideLine);
 
   // render the brass twice, one for each ear ;)
   for (let i = 0; i < 2; i++) {
+    const brassWidth = brassPairWidth * 0.5;
     const startX = math.lerp(
-      startPoint[0] - brassWidth / 4,
-      startPoint[0] + brassWidth / 4,
+      startPoint[0] - brassPairWidth / 4,
+      startPoint[0] + brassPairWidth / 4,
       i
     );
-    console.log(startX);
     const brassStartPoint = [startX, startPoint[1]];
 
     // create and add the hook to the lines accumulator
     // this hook is the same for every brass, so it isn't part of the generator
     const hookLines = [];
-    const hookLength = brassWidth * 0.13;
+    const hookLength = brassPairWidth * 0.13;
     const hookRadius = hookLength * 0.3;
     const hookCircle = createCircleLine(
       brassStartPoint[0] + hookRadius,
@@ -63,7 +114,14 @@ function createBrassLines(
     hookLines.push(hookCircle, hookStem);
     brassLines.push(...hookLines);
 
-    // loop through the brass config and render each piece
+    // recurse through the brass tree and render each piece
+    const brassTreeStartPoint = [
+      brassStartPoint[0],
+      brassStartPoint[1] + hookLength
+    ];
+    brassLines.push(
+      ...createBrassTreeLines(brassTree, brassTreeStartPoint, brassWidth)
+    );
   }
 
   return brassLines;
@@ -74,10 +132,16 @@ const sketch = ({ width, height }) => {
   const margin = Math.max(width, height) / 6;
   const brassPairWidth = width / 10;
 
-  createGrid(5, 5).map(([u, v]) => {
+  const rows = 5;
+  const numLevels = 3;
+  createGrid(rows, rows).map(([u, v]) => {
     const x = math.lerp(margin, width - margin, u);
     const y = math.lerp(margin, height - margin, v);
-    const brassLines = createBrassLines([x, y], brassPairWidth);
+    const brassLines = createPairBrassLines(
+      [x, y],
+      brassPairWidth,
+      generateBrassTree(numLevels)
+    );
     lines.push(...brassLines);
   });
 
