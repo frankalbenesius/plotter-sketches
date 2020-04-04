370571cbd6dd3c02ec3de9f18731e4f77eafbdf6
Frank Albenesius <frankalbenesius@gmail.com>
2020-04-03 19:47:55 -0500

    add stuff

--- sketches/6x4/noisy_lines.js
+++ sketches/6x4/noisy_lines.js
@@ -2,24 +2,29 @@ import canvasSketch from "canvas-sketch";
 import { renderPolylines } from "canvas-sketch-util/penplot";
 import { clipPolylinesToBox } from "canvas-sketch-util/geometry";
 import random from "canvas-sketch-util/random";
-import { lerp } from "canvas-sketch-util/math";
-import range from "lodash/range";
-import {
-  settings,
-  distanceBetweenPoints,
-  createGrid,
-  createNoisyLines,
-  logSeed
-} from "../../../util";
+import { settings, createNoisyLines, logSeed } from "../../util";
 
 const sketch = ({ width, height }) => {
   logSeed();
   let lines = [];
-  const margin = Math.max(width, height) / 30;
+  const margin = 0.125;
 
-  const noisyLines = createNoisyLines([0, 0], [width, height]);
+  const padding = 1.1;
+  const segment = 1;
+  const fill = 1;
+  const freq = 1;
+  const amp = 0.75;
+  const noisyLines = createNoisyLines(
+    [0, 0],
+    [width, height],
+    padding,
+    segment,
+    fill,
+    freq,
+    amp
+  );
 
-  lines.push(...noisyLines.filter(() => random.value() < 0.8));
+  lines.push(...noisyLines.filter(() => random.value() < 1.1));
 
   const box = [margin, margin, width - margin, height - margin];
   lines = clipPolylinesToBox(lines, box);
