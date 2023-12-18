4f7ea3d4c8c131ecb93e6aae628e35c556345aca
Frank Albenesius <frankalbenesius@gmail.com>
2023-12-18 10:42:20 -0600

    Denser circle grid

--- sketches/6x4/circle_grid.js
+++ sketches/6x4/circle_grid.js
@@ -8,13 +8,18 @@ import createCube from "../../util/createCube";
 
 const sketch = ({ width, height }) => {
   let lines = [];
-  const margin = 0.8;
 
-  createGrid(60, 40).map(([u, v]) => {
+  const margin = 0.5;
+  const rows = 48;
+  const freq = 3;
+  const amp = 1;
+
+  const cols = Math.round(rows * 1.5);
+  createGrid(cols, rows).map(([u, v]) => {
     const x = lerp(margin, width - margin, u);
     const y = lerp(margin, height - margin, v);
 
-    const noise = random.noise2D(u, v);
+    const noise = random.noise2D(u, v, freq, amp);
 
     lines.push(createCircleLine(x + noise * margin * 0.15, y, 0.02));
   });
